6160fcd4abd4413397d17d6764dd743933364c76
JakeOleinik <zheka96@gmail.com>
2016-12-07 23:44:56 +0100

    Implemented Contact detail page selection

--- app/src/main/java/com/example/evgeniy/scanner/ContactsFragment.java
+++ app/src/main/java/com/example/evgeniy/scanner/ContactsFragment.java
@@ -1,11 +1,13 @@
 package com.example.evgeniy.scanner;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 public class ContactsFragment extends Fragment{
 
     private ListView contactsView;
+    private final ArrayList<Person> personList = new ArrayList<>();
 
     public ContactsFragment() {
         // Required empty public constructor
@@ -36,14 +39,25 @@ public class ContactsFragment extends Fragment{
         Person p1 = new Person("", "Jack", "Sparrow", "000", "js@mail.com", "addr");
         Person p2 = new Person("", "Queen", "Elizabeth", "111", "qe@mail.com", "Buckingham palace");
 
-        final ArrayList<Person> personList = new ArrayList<>();
         personList.add(p1);
         personList.add(p2);
 
         ContactAdapter adapter = new ContactAdapter(context, personList);
         contactsView.setAdapter(adapter);
 
+        contactsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Person selectedPerson = personList.get(position);
+                Intent detailIntent = new Intent(view.getContext(), ContactDetailActivity.class);
+                detailIntent.putExtra("name", selectedPerson.getFirstName()+" "+selectedPerson.getLastName());
+                startActivity(detailIntent);
+            }
+        });
+
         return view;
     }
+
+
 
 }
